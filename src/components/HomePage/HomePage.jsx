e69a363305c8bc17abe73e3e30e96933d4bfe899
jose urtubey <joseaurtubey@gmail.com>
2023-10-06 14:35:14 -0300

    added chart to homepage for an example view

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,8 +1,46 @@
 import "./HomePage.css";
 
 import React from "react";
+import { BarChart, CartesianAxis, CartesianGrid, XAxis, YAxis, Tooltip,Legend, Bar } from "recharts";
 
 export default function HomePage() {
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+    },
+  ];
   return (
     <body className="body-home">
       <div>
@@ -10,9 +48,19 @@ export default function HomePage() {
       </div>
       <div className="contenedor-linea-home">
         <hr className="linea-home"></hr>
+      <h3 className="subtitle">Acciones Rapidas</h3>
       </div>
-      <h3 className="subtitulo">Acciones Rapidas</h3>
-      <div className="container-slider"></div>
+      <div className="container-chart">
+        <BarChart width={730} height={250} data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
+        </BarChart>
+      </div>
       <div className="contenedor-linea-home">
         <hr className="linea-home"></hr>
       </div>
