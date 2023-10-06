44d6ff4e32f8c8bdcacdb03ba0c9832e26000799
JPGenaro <jpgenaro06@gmail.com>
2023-10-06 15:10:42 -0300

    finish

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,10 +3,11 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 // Estilos para el Navbar
 const navbarStyle = {
-  backgroundColor: 'black', // Color de fondo oscuro
+  backgroundColor: 'white', // Fondo blanco
   position: 'fixed', // Fija el Navbar en la parte superior de la página
   top: 0, // Alinea el Navbar en la parte superior
   left: 0, // Alinea el Navbar desde el borde izquierdo
@@ -22,18 +23,33 @@ const buttonContainerStyle = {
   marginLeft: 'auto', // Alinea los botones a la derecha
 };
 
+// Estilos para el título
+const titleStyle = {
+  color: 'black', // Color de texto negro
+};
+
+// Estilos para los botones
+const buttonStyle = {
+  color: 'black', // Color de texto negro
+};
+
 function Navbar() {
   return (
     <AppBar position="static" style={navbarStyle}>
       <Toolbar>
-        <Typography variant="h6" component="div">
+        <Typography variant="h6" component="div" style={titleStyle}>
           Mi Aplicación
         </Typography>
         <div style={buttonContainerStyle}>
-          <Button color="inherit">Iniciar sesión</Button>
-          <Button color="inherit">Registrarse</Button>
+          <Button color="inherit" style={buttonStyle}>
+            Iniciar sesión
+          </Button>
+          <Button color="inherit" style={buttonStyle}>
+            Registrarse
+          </Button>
         </div>
       </Toolbar>
+      <Box sx={{ bgcolor: 'navy', height: '2px', width: '100%' }}></Box>
     </AppBar>
   );
 }
